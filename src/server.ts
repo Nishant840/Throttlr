8d64612eb723598c598d23e9af44f3ff2151a003
Nishant840 <nishant7896545@gmail.com>
2026-03-31 02:48:30 +0530

    containerizing the application using docker

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,8 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
+const INSTANCE_ID = process.env.INSTANCE_ID || "local";
 
 app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port ${PORT} | Instance: ${INSTANCE_ID}`);
 })
